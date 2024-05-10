eeb60d4671fc71d869441449990fdea2ae02ff34
hariharanboss <153356043+hariharanboss@users.noreply.github.com>
2024-05-10 10:08:15 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -4,5 +4,4 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("█▀▄▀█ █▀█ █░█ █▄░█ ▄▀█ █░░   ▀█▀ █░█ ▄▀█ █▄▀ █░█ █▀█   █▄▄ █▀█ ▀█▀   █ █▀   ▄▀█ █░░ █ █░█ █▀▀
-█░▀░█ █▀▄ █▄█ █░▀█ █▀█ █▄▄   ░█░ █▀█ █▀█ █░█ █▄█ █▀▄   █▄█ █▄█ ░█░   █ ▄█   █▀█ █▄▄ █ ▀▄▀ ██▄")
+    return web.json_response("MrunalThakurBot")
